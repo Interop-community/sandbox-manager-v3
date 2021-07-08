e7dff068db03bf42aeac361136a574dd4bf88996
Gopal Menon <gopal@interopion.com>
2021-07-08 11:29:12 -0600

    New branch for meld

---
+++ xsettings/xsettings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+jq -n --arg gateway_endpoint "$GATEWAY_HOST" \
+--arg auth_endpoint "$AUTH_HOST" \
+--arg ehr_endpoint "$EHR_APP_HOST" \
+--arg patient_data_manager_endpoint "$PATIENT_DATA_MANAGER_HOST" \
+--arg kc_endpoint "$KEYCLOAK_HOST" \
+--arg kc_realm "$KEYCLOAK_REALM" \
+--arg sandbox_endpoint "$SANDBOX_HOST" \
+--arg sandbox_api "$SANDBOX_API" \
+-f ./xsettings_template.json > xsettings.json
+
