aa837e90d5f3b542611e3ae5f585d85f4e8276c9
Chit Zin Win <chit.win@mihin.org>
2021-09-30 16:57:55 -0400

    added conditional rollout restart

--- projects/sandbox-manager/src/assets/strings/index.js
+++ projects/sandbox-manager/src/assets/strings/index.js
@@ -25,7 +25,7 @@ export default {
         mainLabel: 'Sandboxes',
         createLabel: '+ Create Sandbox'
     },
-    defaultSandboxTitle: 'Testing CI/CD',
+    defaultSandboxTitle: '',
     dashboard: 'Dashboard',
     signInLabel: 'Sign In'
 }
