ff21b8929f87d2e32e219d33d4a2cfb37e00b550
Chit Zin Win <chit.win@mihin.org>
2021-09-30 09:47:41 -0400

    adder --region to ecr login

--- ci/ci-3-docker-build-push.sh
+++ ci/ci-3-docker-build-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -t 745222113226.dkr.ecr.us-east-1.amazonaws.com/interop/sandbox-manager-prototype:discovery ..
-aws ecr get-login-password --profile default | docker login --username AWS --password-stdin 745222113226.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 745222113226.dkr.ecr.us-east-1.amazonaws.com
 docker push 745222113226.dkr.ecr.us-east-1.amazonaws.com/interop/sandbox-manager-prototype:discovery
 
