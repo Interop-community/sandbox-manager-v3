60f49fef11b59165f9619c0e94e5bda67ac38e0f
Chit Zin Win <chit.win@mihin.org>
2021-09-29 20:41:21 -0400

    added discovery branch to pipeline

---
+++ ci/ci-3-docker-build-push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker build -t 745222113226.dkr.ecr.us-east-1.amazonaws.com/interop/sandbox-manager-prototype:${BITBUCKET_BRANCH} ..
+aws ecr get-login-password | docker login --username AWS --password-stdin 745222113226.dkr.ecr.us-east-1.amazonaws.com
+docker push 745222113226.dkr.ecr.us-east-1.amazonaws.com/interop/sandbox-manager-prototype:${BITBUCKET_BRANCH}
+
