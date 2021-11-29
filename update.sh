ad83d2a795f5d22f3ac1a7ddb42c3d5e4ac6a68a
Chit Zin Win <chit.win@mihin.org>
2021-11-28 23:24:52 -0500

    testing eventlistener

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ACCOUNT=745222113226
+export AWS_PROFILE=iol
+
+docker build -t ${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/interop/sandbox-manager-prototype:latest .
+cliversion=$(aws --version)
+[[ $cliversion = aws-cli/1* ]] && aws ecr get-login --no-include-email --region us-east-1 | source /dev/stdin
+[[ $cliversion = aws-cli/2* ]] && aws ecr get-login-password | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com
+docker push ${ACCOUNT}.dkr.ecr.us-east-1.amazonaws.com/interop/sandbox-manager-prototype:latest
+
+unset AWS_PROFILE
